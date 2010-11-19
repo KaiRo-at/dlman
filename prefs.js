61598be3c5c1d72cf1ac3361090b35ffdf1c1375
Robert Kaiser <kairo@kairo.at>
2010-11-20 00:33:18 +0100

    add prefs

---
+++ prefs.js
@@ -0,0 +1,6 @@
+// 0 opens the download manager
+// 1 opens a progress dialog
+// 2 and other values, no download manager, no progress dialog.
+pref("browser.download.manager.behavior", 0);
+
+pref("browser.download.progress.closeWhenDone", false);
